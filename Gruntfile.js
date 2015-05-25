1a5f37d171f6cc8e74a6dd0d22fcd1847cab03eb
lijian <aimilixi@foxmail.com>
2015-05-25 10:47:34 +0800

    fix	--gruntfile.js 的host name 可能会导致启动web服务器失败

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function (grunt) {
         open: true,
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
-        hostname: '192.168.1.110'
+        hostname: 'localhost'
       },
       livereload: {
         options: {
